e54b16e4ca04e67873dda625b739af4058818bb0
FuryAryan <yoyo26new@gmail.com>
2020-03-24 23:43:31 +0530

    changes in Blockchain.py

--- Module 1 : Creating BlockChain/blockchain.py
+++ Module 1 : Creating BlockChain/blockchain.py
@@ -65,13 +65,35 @@ class BlockChain:
 #creating a web app
 app = Flask(__name__)
 
-#creating a blockchain
+#creating a blockchain 
 blockchain  = Blockchain() 
 
 
 #Mining a new block 
 @app.route('/mine_block', methods = ['GET'])
-def mine_block(): 
+def mine_block():#this function will mine a block 
+    previous_block = blockchain.get_previous_block() 
+    previous_proof = previous_block('proof')
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof,previous_hash)
+    response = {'message':'Congrats You just mined a block!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+# Getting the full Blockchain
+
+@app.route('/get_chain', methods = ['GET']) 
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)} 
+    
+    
+
+    
     
     
 
